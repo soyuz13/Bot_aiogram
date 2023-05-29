25425d248b90d4aea4de2dbb47f1e9d1445b46df
Vadim <transit010@mail.ru>
2023-05-30 06:48:13 +1000

    7commit

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ load_dotenv()
 
 TOKEN = os.getenv('TLG_TOKEN')
 CHAT_ID = os.getenv('TLG_CHAT_ID')
-APP_BASE_URL = 'https://0a3b-2a00-1fa3-635-93a0-5480-e27b-2019-5d07.ngrok-free.app'
-# APP_BASE_URL = 'https://myteatime.ru'
+APP_BASE_URL = 'https://176.124.192.33'
